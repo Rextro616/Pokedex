dc9f47b651c8634c88acd97da9d6d4a11419b899
Roxana Flores <223275@ids.upchiapas.edu.mx>
2023-09-26 02:27:47 -0600

    diosmio ayuda

--- pokedex/src/components/organisms/Modal/Modal.tsx
+++ pokedex/src/components/organisms/Modal/Modal.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import SquareImg from "../../atoms/SquareImg/SquareImg";
+import "./Modal.css";
+import PokemonCard from "../../molecules/PokemonCard/PokemonCard";
 
-export default function Modal() {
+const OVERLAY_STYLES = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "#5694D7",
+  zIndex: 1000,
+};
+
+export default function Modal({ open, children, onClose, pokemon }) {
+  console.log(pokemon);
+  if (!open) return true;
+
   return (
-    <div>Modal</div>
-  )
+    <div style={OVERLAY_STYLES}>
+      <div className="styles" onClick={onClose}>
+        {children}
+        <SquareImg img={pokemon.sprites.front_default} />
+        <PokemonCard
+          num={"N. ° " + pokemon.id}
+          nombre={pokemon.name}
+          img={pokemon.sprites.front_default}
+          color="#E99441"
+        />
+      </div>
+    </div>
+  );
 }
--- pokedex/src/components/organisms/PokeList/PokeList.tsx
+++ pokedex/src/components/organisms/PokeList/PokeList.tsx
@@ -1,18 +1,42 @@
-import './PokeList.css'
-import PokemonCard from '../../molecules/PokemonCard/PokemonCard'
+import "./PokeList.css";
+import PokemonCard from "../../molecules/PokemonCard/PokemonCard";
+import { useState } from "react";
+import Modal from "../Modal/Modal";
+import Carousel from "../Carousel/Carousel";
 
 function PokeList(pokemon: { pokemon: any[] }) {
+  const [isOpen, setIsOpen] = useState(false);
+  let [pokemonChosed, setpokemonChosed] = useState();
 
   return (
-    <div id='pokeList' className='pokeList'>
-    
-        {pokemon.pokemon.map(p =>(
-            <div id={p.name} className='pokeCarousel'>
-                <PokemonCard img={p.sprites.front_default} color='#EEDEAB' num={'N. ° ' + p.id} nombre={p.name}/>
-            </div>
+    <div id="pokeList" className="pokeList">
+      <Carousel>
+        {pokemon.pokemon.map((p) => (
+          <div
+            id={p.name}
+            onClick={() => {
+              setIsOpen(true);
+              setpokemonChosed(p);
+            }}
+            className="pokeCarousel"
+          >
+            <PokemonCard
+              img={p.sprites.front_default}
+              color="#EEDEAB"
+              num={"N. ° " + p.id}
+              nombre={p.name}
+            />
+          </div>
         ))}
+      </Carousel>
+
+      <Modal
+        pokemon={pokemonChosed}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      ></Modal>
     </div>
-  )
+  );
 }
 
-export default PokeList
+export default PokeList;
